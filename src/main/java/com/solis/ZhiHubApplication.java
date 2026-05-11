f1bb5b1f63bc63a2b851720775e44208fe6c396f
Solis <16898659+epoch2@user.noreply.gitee.com>
2026-05-11 14:46:43 +0800

    初始化

---
+++ src/main/java/com/solis/ZhiHubApplication.java
@@ -0,0 +1,12 @@
+package com.solis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiHubApplication.class, args);
+    }
+}
+
